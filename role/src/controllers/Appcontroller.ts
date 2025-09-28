c0ea99f18b85308752734e40ee5ad7c02535c2d9
estiven dubon <stivdub01@gmail.com>
2025-09-28 15:35:14 -0600

    [update]: roles fijos

--- role/src/controllers/Appcontroller.ts
+++ role/src/controllers/Appcontroller.ts
@@ -92,6 +92,11 @@ export class AppController {
                 return res.status(400).json({ message: "Faltan parámetros obligatorios" });
             }
 
+            //validar que el rol no sea 1, 2 o 3 (admin, supervisor, tecnico)
+            if (roleId === 1 || roleId === 2 || roleId === 3) {
+                return res.status(400).json({ message: "No es posible eliminar este rol por funcionalidad del sistema" });
+            }
+
             // Verificar si el rol existe
             const existingRole = await roleService.findRoleById(roleId);
             if (!existingRole) {
@@ -170,6 +175,11 @@ export class AppController {
             const currentUser = res.locals.currentUser;
             if (!roleId) {
                 return res.status(400).json({ message: "Faltan parámetros obligatorios" });
+            }
+
+            //validar que el rol no sea 1, 2 o 3 (admin, supervisor, tecnico)
+            if (roleId === 1 || roleId === 2 || roleId === 3) {
+                return res.status(400).json({ message: "No es posible eliminar este rol por funcionalidad del sistema" });
             }
             // Verificar si el rol existe
             const existingRole = await roleService.findRoleById(roleId);
