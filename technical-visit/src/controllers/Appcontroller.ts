45304fb3140d7f6ef88215b213ad69f3419d3dbc
estiven dubon <stivdub01@gmail.com>
2025-09-30 21:26:32 -0600

    [update]: listado de visitas tecnicas por permisos

--- technical-visit/src/controllers/Appcontroller.ts
+++ technical-visit/src/controllers/Appcontroller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import { VisitService } from '../services/visit.service';
+import { AuthService } from '../services/auth.service';
 
 const visitService = new VisitService();
+const authService = new AuthService();
 
 export class AppController {
 
@@ -12,23 +14,35 @@ export class AppController {
             const currentUser = res.locals.currentUser;
             console.log("Usuario actual:", currentUser);
 
-            if (currentUser.roleId === 2) { //SUPERVISOR
+            const permissions = await authService.getPermissionByRole(currentUser.roleId);
+
+            //validar que tenga el permiso show-visit-gen para listar todas las visitas tecnicas - administrador general
+            const hasGeneralPermission = permissions?.some((perm: any) => perm.CODE === 'show-visit-gen');
+            if (hasGeneralPermission) {
+                console.log("listando todas las visitas tecnicas - general");
+                const visits = await visitService.getAllTechnicalVisitsGeneral();
+                return res.status(200).json({ data: visits });
+            }
+
+
+            //validar que tenga el permiso show-visit-sup para listar todas las visitas tecnicas - supervisor
+            const hasSupervisorPermission = permissions?.some((perm: any) => perm.CODE === 'show-visit-sup');
+            if (hasSupervisorPermission) { //SUPERVISOR
                 console.log("listando todas las visitas tecnicas - supervisor");
                 const visits = await visitService.getAllTechnicalVisitsBySupervisor(currentUser.userId);
                 return res.status(200).json({ data: visits });
             }
 
-            if (currentUser.roleId === 3) { //TECNICO
+            //validar que tenga el permiso show-visit-tec para listar todas las visitas tecnicas - tecnico
+            const hasTechnicianPermission = permissions?.some((perm: any) => perm.CODE === 'show-visit-tec');
+            if (hasTechnicianPermission) { //TECNICO
                 console.log("listando todas las visitas tecnicas - tecnico");
                 const visits = await visitService.getAllTechnicalVisitsByTechnician(currentUser.userId);
                 return res.status(200).json({ data: visits });
             }
-            //ADMINISTRADOR - general
-            console.log("listando todas las visitas tecnicas - general");
-            const clients = await visitService.getAllTechnicalVisitsGeneral();
-            return res.status(200).json({ data: clients });
+            return res.status(403).json({ message: "No tiene permisos para ver las visitas técnicas" });
         } catch (error) {
-            console.error("Error al obtener los clientes:", error);
+            console.error("Error al obtener las visitas técnicas:", error);
             return res.status(500).json({ message: "Error interno del servidor", error: error.message });
         }
     }
